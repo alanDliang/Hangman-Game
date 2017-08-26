80060a8307a7109b26740d53df4d6a05f728c49d
Alan Liang <ariez84@gmail.com>
2017-08-26 17:23:44 -0400

    first

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//1. make array of words for game.
+//2. select random word from array.
+//3. users guessed letters will be displayed on screen
+//4. match guess letters to random seleted word.
+//5. 
+
+var moviesList = ['titanic', 'fight club', 'saving private ryan', 'forrest gump', 'office space', 'the shawshank redemption', 'american beauty', 'mulan', 'armageddon', 'schindlers list', 'pulp fiction', 'goodfellas']
