f7107a8625a234822cb0ce7ad8310471cd538ba9
Alan Liang <ariez84@gmail.com>
2017-08-28 06:22:59 -0400

    second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,52 @@
 //1. make array of words for game.
 //2. select random word from array.
-//3. users guessed letters will be displayed on screen
-//4. match guess letters to random seleted word.
-//5. 
+//3. # of letters from random movie = "_".
+//4. empty string = random movie.
+//5. join
 
 var moviesList = ['titanic', 'fight club', 'saving private ryan', 'forrest gump', 'office space', 'the shawshank redemption', 'american beauty', 'mulan', 'armageddon', 'schindlers list', 'pulp fiction', 'goodfellas']
-var randomPick = moviesList[Math.floor(Math.random() * moviesList.length)];
+var randomWord = moviesList[Math.floor(Math.random() * moviesList.length)];
 
-//function randomWord (targetWord) {
-//	targetWord = moviesList[Math.random()*moviesList.length];
-//}ovie
+var s;
+var count = 0;
+var answerArray = [];
+
+function startUp() {
+	for (var i = 0; i < randomWord.length; i++) {
+	answerArray[i] = "_";
+}
+
+s = answerArray.join(" ");
+document.getElementById("answer").innerHTML = s;
+}
+
+function Letter() {
+	var letter = document.getElementById("letter").value;
+	if (letter.length > 0) {
+		for (var i = 0; i < randomWord.length; i++)
+	
+			if (randomWord[i] === letter) {
+				answerArray[i] = letter;
+
+			}		
+}	
+
+function ClearFields() {
+
+     document.getElementById("letter").value = "";
+     
+}
+
+
+count++;
+document.getElementById("counter").innerHTML = "Number of Tries: " + count;
+document.getElementById("answer").innerHTML = answerArray.join(" ");
+}
+if (count>5) {
+	alert("YOU LOSE!");
+	
+	}
+
+
+
+
